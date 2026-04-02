a5cb954f7c3e9dc05c94554d9bfb00d3c6cc0bcc
KumarAyush000 <krayush123000@gmail.com>
2026-04-02 09:54:54 +0530

    implemented SavingsAccount subclass with interest credit functionality, fixed the balance assignment as it is private

--- SavingsAccount.py
+++ SavingsAccount.py
@@ -5,6 +5,6 @@ class SavingsAccount(BankAccount):
 
     def apply_iterest(self):
         interest = (self.balance * self.interest_rate) /100
-        self.balance += interest
+        self._balance += interest
         self._add_transaction("INTEREST_CREDITED", interest)
         print(f"Interest of {interest} applied to {self.account_number}")
